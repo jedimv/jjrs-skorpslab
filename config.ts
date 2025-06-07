587a5333cb8cd1ccb985cfb3e966aa21a1d1a65e
jedimv <113726514+jedimv@users.noreply.github.com>
2025-06-07 15:48:03 -0300

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,8 @@
+// Change this file name to config.ts and fill with your values
+
+export default {
+  roomName: `RS TESTE - favor nao entrar`,
+  public: true,
+  maxPlayers: 30,
+  token: `thr1.AAAAAGhEfYI64xnrE0Iqeg.9Ajqsx3IU7Y`, // paste your token from https://haxball.com/headlesstoken
+};
